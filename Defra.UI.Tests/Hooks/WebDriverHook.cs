3cba1d4f9c7d5d69f181300fe248ade95a8b18d3
Subbiah, Nagarajan <Nagarajan.Subbiah@defra.gov.uk>
2024-12-19 11:27:10 +0000

    Implement extent reports

--- Defra.UI.Tests/Hooks/WebDriverHook.cs
+++ Defra.UI.Tests/Hooks/WebDriverHook.cs
@@ -1,10 +1,13 @@
-﻿using BoDi;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin;
+using BoDi;
 using Capgemini.PowerApps.SpecFlowBindings.Hooks;
 using Defra.UI.Framework.Driver;
 using Defra.UI.Framework.Object;
 using Defra.UI.Tests.Capabilities;
 using Defra.UI.Tests.Configuration;
 using Defra.UI.Tests.HelperMethods;
+using Defra.UI.Tests.Tools;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using System.Net.Http.Headers;
@@ -28,12 +31,30 @@ namespace Defra.UI.Tests.Hooks
         private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
         private IFetchCodeFromEmail FetchCodeFromEmail => _objectContainer.IsRegistered<IFetchCodeFromEmail>() ? _objectContainer.Resolve<IFetchCodeFromEmail>() : null;
 
+        private static ExtentReports _extent;
+        [ThreadStatic]
+        private static ExtentTest _feature;
+        [ThreadStatic]
+        private static ExtentTest _scenario;
+
         public WebDriverHook(ScenarioContext context, ObjectContainer container,
             ISpecFlowOutputHelper specFlowOutputHelper)
         {
             _scenarioContext = context;
             _objectContainer = container;
             _specFlowOutputHelper = specFlowOutputHelper;
+        }
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            _extent = ExtentReportManager.GetInstance();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            _feature = _extent.CreateTest<AventStack.ExtentReports.Gherkin.Model.Feature>(featureContext.FeatureInfo.Title);
         }
 
         [BeforeScenario(Order = (int)HookRunOrder.WebDriver)]
@@ -51,6 +72,8 @@ namespace Defra.UI.Tests.Hooks
             }
 
             _objectContainer.RegisterInstanceAs(Driver);
+
+            _scenario = _feature.CreateNode<AventStack.ExtentReports.Gherkin.Model.Scenario>(_scenarioContext.ScenarioInfo.Title);
         }
 
         [AfterScenario]
@@ -79,6 +102,8 @@ namespace Defra.UI.Tests.Hooks
                 }
 
                 CloseBrowsers();
+
+                _extent.Flush();
             }
         }
 
@@ -127,6 +152,43 @@ namespace Defra.UI.Tests.Hooks
             {
                 Logger.LogMessage($"Not able to print Node information for {endpoint}, most likely running against manually started appium server.");
             }
+        }
+
+        [AfterStep]
+        public void AfterStep()
+        {
+            var stepType = _scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+            var stepInfo = _scenarioContext.StepContext.StepInfo.Text;
+            var screenshotPath = CaptureScreenshot();
+
+            if (_scenarioContext.TestError == null)
+            {
+                _scenario
+                    .CreateNode(new GherkinKeyword(stepType), stepInfo)
+                    .Pass("Step passed")
+                    .AddScreenCaptureFromPath(screenshotPath);
+            }
+            else
+            {
+                _scenario.CreateNode(new GherkinKeyword(stepType), stepInfo)
+                         .Fail(_scenarioContext.TestError.Message)
+                         .AddScreenCaptureFromPath(screenshotPath);
+            }
+        }
+
+        private string CaptureScreenshot()
+        {
+            var screenshotsDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Reports", "Screenshots");
+
+            if (!Directory.Exists(screenshotsDir))
+            {
+                Directory.CreateDirectory(screenshotsDir);
+            }
+
+            var screenshot = ((ITakesScreenshot)Driver).GetScreenshot();
+            var filePath = Path.Combine(screenshotsDir, $"{Guid.NewGuid()}.png");
+            screenshot.SaveAsFile(filePath);
+            return filePath;
         }
 
         private void CloseBrowsers()
