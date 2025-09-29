cd4a4063f46c1d78b9024e0552799513f61044ac
nagarajansubbiah25 <nagarajan.subbiah@defra.gov.uk>
2025-09-29 15:21:21 +0100

    fix compilation error

--- Defra.UI.Tests/Hooks/WebDriverHook.cs
+++ Defra.UI.Tests/Hooks/WebDriverHook.cs
@@ -75,7 +75,7 @@ namespace Defra.UI.Tests.Hooks
             {
                 var reportPath = Path.Combine($"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}", "Accessibility");
                 Console.WriteLine(reportPath);
-                Cognizant.WCAG.Compliance.Checker.Start.Init(Driver, reportPath, false);
+                Cognizant.WCAG.Compliance.Checker.Start.Init(Driver, reportPath);
             }
 
             _scenario = _feature.CreateNode<AventStack.ExtentReports.Gherkin.Model.Scenario>(_scenarioContext.ScenarioInfo.Title);
