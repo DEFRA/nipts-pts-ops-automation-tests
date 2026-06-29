8e4d2050adcdcf96761bd1fb04205c727b2c0df8
Rasiya Pareeth <Rasiya.Pareeth@defra.gov.uk>
2026-06-29 16:29:54 +0100

    Code fixes for AP pipeline failure

--- Defra.UI.Tests/Pages/WELSH/Classes/HomePageWelsh.cs
+++ Defra.UI.Tests/Pages/WELSH/Classes/HomePageWelsh.cs
@@ -49,7 +49,7 @@ namespace Defra.UI.Tests.Pages.WELSH.Classes
         private IReadOnlyCollection<IWebElement> txtStausValues => _driver.WaitForElements(By.XPath("//*[@class = 'govuk-table__row']/td[1]"));
         private IReadOnlyCollection<IWebElement> txtViewLinks => _driver.WaitForElements(By.XPath("//*[@class = 'govuk-table__row']/td[2]"));
         private IReadOnlyCollection<IWebElement> lnksManageAccAndSingOut => _driver.FindElements(By.XPath("//div[@class = 'login-nav govuk-!-display-none-print']"));
-        private IWebElement lblSusWarning => _driver.WaitForElement(By.XPath("//div[@class = 'govuk-warning-text']/strong/text()[2]"));
+        private IWebElement lblSusWarning => _driver.WaitForElement(By.XPath("//div[@class = 'govuk-warning-text']/strong"));
         private IReadOnlyCollection<IWebElement> btnApplyForDocumentCheck => _driver.FindElements(By.XPath("//button[normalize-space(text())='Gwneud cais am ddogfen']"));
         private IReadOnlyCollection<IWebElement> lblSusStatusInDashboard => _driver.FindElements(By.XPath("//*[@class='govuk-table__cell status-column']/strong"));
         private IWebElement lblCookiesBanner => _driver.WaitForElement(By.XPath("//h2[@class = 'govuk-cookie-banner__heading govuk-heading-m']"));
@@ -302,7 +302,9 @@ namespace Defra.UI.Tests.Pages.WELSH.Classes
 
         public bool VerifySuspensionWarningInWelsh()
         {
-            return lblSusWarning.Text.Trim().Contains("Rydych chi wedi cael eich atal o'r cynllun yma a chewch chi ddim defnyddio’ch dogfennau teithio anifeiliaid anwes na gwneud cais am rai newydd nes bod eich ataliad wedi’i godi. Gwiriwch eich ebost am ragor o wybodaeth, gan gynnwys sut i apelio.");
+            var element = lblSusWarning;
+            var text = element.Text.Trim(); // Gets all text content from the <strong> element
+            return text.Contains("Rydych chi wedi cael eich atal o'r cynllun yma a chewch chi ddim defnyddio’ch dogfennau teithio anifeiliaid anwes na gwneud cais am rai newydd nes bod eich ataliad wedi’i godi. Gwiriwch eich ebost am ragor o wybodaeth, gan gynnwys sut i apelio.");           
         }
 
         public bool VerifyWelshApplyButtonNotVisible()
