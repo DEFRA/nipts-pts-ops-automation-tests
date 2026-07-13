6d57dabb03d75bb17d6343226f10164997c766ba
Rasiya Pareeth <Rasiya.Pareeth@defra.gov.uk>
2026-07-13 12:43:35 +0100

    reverting the changes

--- Defra.UI.Tests/Capabilities/BrowserStackCapability.cs
+++ Defra.UI.Tests/Capabilities/BrowserStackCapability.cs
@@ -2,8 +2,6 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Safari;
-using System.Globalization;
 using Reqnroll;
 
 namespace Defra.UI.Tests.Capabilities
@@ -12,9 +10,9 @@ namespace Defra.UI.Tests.Capabilities
     {
         private BaseConfiguration _configuration => ConfigSetup.BaseConfiguration;
         private readonly ScenarioContext _scenarioContext;
-        private readonly Dictionary<string, object> _capDictionary = new Dictionary<string, object>();
-        private readonly Dictionary<string, object> _browserstackOptions = new Dictionary<string, object>();
-        private static readonly string[] _osList = new[] { "WINDOWS", "OS X" };
+        private readonly Dictionary<string, object> _capDictionary = [];
+        private readonly Dictionary<string, object> _browserstackOptions = [];
+        private static readonly string[] _osList = ["WINDOWS", "OS X"];
 
         private readonly string _target;
         private readonly string _deviceName;
@@ -32,37 +30,28 @@ namespace Defra.UI.Tests.Capabilities
 
         public DriverOptions GetDriverOptions(Dictionary<string, string> capDictionary = null)
         {
-            // populate common BrowserStack options
             GetBrowserStackConfig();
             GetProjectDriverOptions();
             GetTestNameDriverOptions();
 
-            _browserstackOptions["acceptInsecureCerts"] = true;
-            _capDictionary["autoGrantPermission"] = true;
+            _browserstackOptions.Add("acceptInsecureCerts", true);
 
-            var deviceUpper = _deviceName?.ToUpperInvariant() ?? string.Empty;
-            var browserName = string.IsNullOrWhiteSpace(_target)
-                ? "Chrome"
-                : CultureInfo.InvariantCulture.TextInfo.ToTitleCase(_target.ToLowerInvariant());
+            _capDictionary.Add("autoGrantPermission:", true);
+            _capDictionary.Add("osVersion", _bs_os_version);
 
-            // Mobile device configuration
-            if (deviceUpper.Contains("IPAD") || deviceUpper.Contains("IPHONE"))
+            if (_osList.Contains(_deviceName.ToUpper()))
             {
-                // BrowserStack expects device info in bstack:options but top-level W3C keys should also be set
-                _browserstackOptions["deviceName"] = _deviceName;
-                if (!string.IsNullOrEmpty(_bs_os_version)) _browserstackOptions["osVersion"] = _bs_os_version;
-                _browserstackOptions["realMobile"] = true;
-
-                // Use SafariOptions for iOS runs and set top-level W3C capabilities
-                var opts = new SafariOptions();
-                AddDictionaryValuesInDriverOptions(opts, _capDictionary);
-                // set the browserName as an additional option (setter is inaccessible on DriverOptions)
-                opts.AddAdditionalOption("browserName", "Safari");
-                // platformName is a W3C capability; keep as additional option if needed
-                opts.AddAdditionalOption("platformName", "iOS");
-                // also keep browserstack-specific options nested
-                opts.AddAdditionalOption("bstack:options", _browserstackOptions);
-                return opts;
+                _capDictionary.Add("os", _deviceName);
+                _browserstackOptions.Add("os", _deviceName);
+                _browserstackOptions.Add("browser", _target);
+                _browserstackOptions.Add("browserVersion", _bs_browser_version);
+            }
+            else
+            {
+                _capDictionary.Add("deviceName", _deviceName);
+                _browserstackOptions.Add("deviceName", _deviceName);
+                _browserstackOptions.Add("browserName", _target);
+                _browserstackOptions.Add("deviceOrientation", "portrait");
             }
 
             _browserstackOptions.Add("local", "false");
@@ -80,12 +69,7 @@ namespace Defra.UI.Tests.Capabilities
             AddDictionaryValuesInDriverOptions(driverOptions, _capDictionary);
             driverOptions.AddAdditionalOption("bstack:options", _browserstackOptions);
 
-            var chromeOpts = new ChromeOptions();
-            AddDictionaryValuesInDriverOptions(chromeOpts, _capDictionary);
-            // set the browserName as an additional option (setter is inaccessible on DriverOptions)
-            chromeOpts.AddAdditionalOption("browserName", browserName);
-            chromeOpts.AddAdditionalOption("bstack:options", _browserstackOptions);
-            return chromeOpts;
+            return driverOptions;
         }
 
         private void GetBrowserStackConfig()
@@ -124,10 +108,6 @@ namespace Defra.UI.Tests.Capabilities
             {
                 foreach (var androidDictionary in capDictionary)
                 {
-                    // avoid adding browserName as an additional option; use DriverOptions.BrowserName instead
-                    if (string.Equals(androidDictionary.Key?.ToString(), "browserName", StringComparison.OrdinalIgnoreCase))
-                        continue;
-
                     driverOptions.AddAdditionalOption(androidDictionary.Key.ToString(), androidDictionary.Value);
                 }
             }
